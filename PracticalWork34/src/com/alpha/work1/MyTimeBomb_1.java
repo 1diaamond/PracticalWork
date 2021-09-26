504c6f8961c195b9f09c0f6301500a983753c78c
Goo <1diaamond@gmail.com>
2021-09-26 22:28:36 +0300

    PracticalWork34 work1 added sleep

--- PracticalWork34/src/com/alpha/work1/MyTimeBomb_1.java
+++ PracticalWork34/src/com/alpha/work1/MyTimeBomb_1.java
@@ -6,7 +6,13 @@ import java.util.stream.Stream;
 public class MyTimeBomb_1 extends Thread{
     @Override
     public void run() {
-        IntStream.rangeClosed(0,10).forEach(i -> System.out.println(10-i));
+        IntStream.rangeClosed(0,10).forEach(i -> {System.out.println(10-i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
         System.out.println("Boom!!!");
     }
 }
--- PracticalWork34/src/com/alpha/work1/MyTimeBomb_2.java
+++ PracticalWork34/src/com/alpha/work1/MyTimeBomb_2.java
@@ -5,7 +5,16 @@ import java.util.stream.IntStream;
 public class MyTimeBomb_2 implements Runnable{
     @Override
     public void run() {
-        IntStream.rangeClosed(0,10).forEach(System.out::println);
+        IntStream.rangeClosed(0,10).forEach(x -> {
+            System.out.println(10-x);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
         System.out.println("Booooooooooooooom!!!");
+
     }
 }
